0cd06925d3e4d3834d56c6e8501ae96b3d8e471a
Filipe Scaglia <filipecscaglia@gmail.com>
2025-01-18 22:33:51 -0300

    crm-1: register form front-end

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-</div>
+<x-card shadown class="mx-auto w-[350px]">
+    <x-form wire:submit="submit">
+        <x-input label="Name" wire:model="name" />
+        <x-input label="Email" wire:model="email" />
+        <x-input label="Confirm your email" wire:model="email_confirmation" />
+        <x-input label="Password" wire:model="password" type="password" />
+    
+        <x-slot:actions>
+            <x-button label="Reset" type="reset" />
+            <x-button label="Register" class="btn-primary" type="submit" spinner="submit" />
+        </x-slot:actions>
+    </x-form>
+</x-card>
