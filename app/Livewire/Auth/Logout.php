3c988e2af3d452f8212eb99695fbc52bc0443338
Filipe Scaglia <filipecscaglia@gmail.com>
2025-01-25 20:20:27 -0300

    crm-5: frontend of our logout button

--- app/Livewire/Auth/Logout.php
+++ app/Livewire/Auth/Logout.php
@@ -9,7 +9,15 @@ class Logout extends Component
 {
     public function render()
     {
-        return view('livewire.auth.logout');
+        return <<<BLADE
+            <div>
+                <x-button
+                    icon="o-power"
+                    class="btn-circle btn-ghost btn-xs"
+                    wire:click="logout"
+                />
+            </div>
+        BLADE;
     }
 
     public function logout(): void
