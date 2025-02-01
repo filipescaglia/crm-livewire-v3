d1d7eb578c4c0729abc407a253fbe2ac3587c193
Filipe Scaglia <filipecscaglia@gmail.com>
2025-02-01 18:20:22 -0300

    crm-7: creating a token when asking to recover the password

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
 Route::get('/logout', fn () => Auth::logout());
 Route::get('/password/recovery', Password\Recovery::class)->name('password.recovery');
+Route::get('/password/reset', fn () => 'oi')->name('password.reset');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
