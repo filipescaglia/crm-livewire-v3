916ee175c5ff70d71cf7d60a6d50de2621e3a7b9
Filipe Scaglia <filipecscaglia@gmail.com>
2025-02-04 21:12:23 -0300

    crm-7: making sure that password is being updated

--- app/Livewire/Auth/Password/Reset.php
+++ app/Livewire/Auth/Password/Reset.php
@@ -2,17 +2,29 @@
 
 namespace App\Livewire\Auth\Password;
 
-use Illuminate\Support\Facades\{DB, Hash};
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\{DB, Hash, Password};
+use Illuminate\Support\Str;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Reset extends Component
 {
-    public ?string $token = null;
+    public string $token;
 
-    public function mount(string $token)
+    public ?string $email = null;
+
+    public ?string $email_confirmation = null;
+
+    public ?string $password = null;
+
+    public ?string $password_confirmation = null;
+
+    public function mount(string $token, ?string $email = null)
     {
         $this->token = $token;
+        $this->email = request()->get('email', $email);
 
         if ($this->isTokenInvalid()) {
             session()->flash('status', 'Token Invalid');
@@ -37,5 +49,28 @@ class Reset extends Component
         }
 
         return true;
+    }
+
+    public function updatePassword()
+    {
+        $status = Password::reset(
+            $this->only(
+                'email',
+                'password',
+                'password_confirmation',
+                'token'
+            ),
+            function (User $user, string $password) {
+                $user->password       = $password;
+                $user->remember_token = Str::random(60);
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        session()->flash('status', trans($status));
+
+        $this->redirect(route('dashboard'));
     }
 }
